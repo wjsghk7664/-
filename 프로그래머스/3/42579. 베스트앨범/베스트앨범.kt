dc23e2835b45b63eba71c1fe4abd7404b74e12b4
wjsghk7664 <83314518+wjsghk7664@users.noreply.github.com>
2024-11-07 15:51:39 +0900

    [level 3] Title: 베스트앨범, Time: 30.47 ms, Memory: 63.8 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.kt
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.kt
@@ -1,26 +1,31 @@
 class Solution {
     fun solution(genres: Array<String>, plays: IntArray): IntArray {
-        var answer = intArrayOf()
-        
-        //장르별로 나누기
-        val map= HashMap<String,ArrayList<Pair<Int,Int>>>()
-        
-        for((idx,v) in genres.withIndex()){
-            val list = map.getOrDefault(v,ArrayList<Pair<Int,Int>>())
-            map.put(v,list.apply{add(Pair(idx,plays[idx]))})
-            
+        var answer = mutableListOf<Int>()
+        val total =hashMapOf<String,Int>()
+        for((i,n) in genres.withIndex()){
+            if(!total.contains(n)){
+                total.put(n,0)
+            }
+            total[n]=total[n]!!+plays[i]
         }
-        //각 장르내의 앨범들을 재생횟수-인덱스 순으로 정렬해서 장르별 리스트를 결과에 넣기
-        val result=ArrayList<List<Pair<Int,Int>>>()
-        for((k,v) in map){
-            result+=v.sortedWith(compareBy<Pair<Int,Int>>{-it.second}.thenBy{it.first})
+        var list = mutableListOf<Pair<String, Int>>()
+        for((k,v) in total){
+            list+=Pair(k,v)
         }
-        //각 장르들을 총 재생횟수기준으로 정렬 
-        result.sortByDescending{list -> list.map{it.second}.sum()}
-        
-        //각 장르별로 곡을 2개 이하씩만 뺀 뒤 flatten으로 한 리스트로 합친 뒤 각 곡의 인덱스만 남겨서 IntArray로 만든다.
-        answer= result.map{it.slice(0..minOf(1,it.size-1))}.flatten().map{it.first}.toIntArray()
-        
-        return answer
+        list = list.sortedByDescending{it.second}.toMutableList()
+        println(list.toString())
+        for(i in list){
+            val gen = i.first
+            var tmplist = mutableListOf<Pair<Int,Int>>()
+            for((idx,n) in genres.withIndex()){
+                if(n==gen){
+                    tmplist+=Pair(idx, plays[idx])
+                }
+            }
+            println(tmplist.toString())
+            tmplist= tmplist.sortedByDescending{it.second}.toMutableList()
+            answer+=if(tmplist.size>=2) tmplist.map{it.first}.slice(0..1) else tmplist.map{it.first}
+        }
+        return answer.toIntArray()
     }
 }
