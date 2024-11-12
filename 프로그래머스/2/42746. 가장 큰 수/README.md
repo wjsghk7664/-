0ac3793260d6c3e418ae247a9fea6c4350ac1bee
wjsghk7664 <83314518+wjsghk7664@users.noreply.github.com>
2024-11-12 21:40:56 +0900

    [level 2] Title: 가장 큰 수, Time: 2748.35 ms, Memory: 373 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/README.md
+++ 프로그래머스/2/42746. 가장 큰 수/README.md
@@ -1,10 +1,10 @@
 # [level 2] 가장 큰 수 - 42746 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42746#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42746) 
 
 ### 성능 요약
 
-메모리: 65.8 MB, 시간: 35.48 ms
+메모리: 373 MB, 시간: 2748.35 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 07월 05일 13:13:06
+2024년 11월 12일 21:40:54
 
 ### 문제 설명
 
--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.kt
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.kt
@@ -1,20 +1,27 @@
 class Solution {
     fun solution(numbers: IntArray): String {
         if(numbers.sum()==0) return "0"
- 
-        var num=Array(numbers.size){Pair(-1,-1)} //바꾼수, 원래수
-        for((idx,i) in numbers.withIndex()){
-            if(i<10){
-                num[idx]=Pair(111111*i,i)
+        
+        val conv = ArrayList<Pair<Int,Int>>()
+        for(i in numbers){
+            if(i==0){
+                conv+=Pair(0,0)
+            }
+            else if(i<10){
+                conv+=Pair(i*1111, i)
             }else if(i<100){
-                num[idx]=Pair(10101*i,i)
+                conv+=Pair(i*100+i,i)
             }else if(i<1000){
-                num[idx]=Pair(1001*i,i)
+                conv+=Pair(i*10+i/100,i)
             }else{
-                num[idx]=Pair(i,i)
+                conv+=Pair(1000,1000)
             }
         }
-        return num.sortedByDescending{it.first}.map{it.second.toString()}.joinToString().replace(", ","")
-        
+        conv.sortWith{a,b -> b.first - a.first}
+        var answer =""
+        for(i in conv){
+            answer+=i.second.toString()
+        }
+        return answer
     }
 }
