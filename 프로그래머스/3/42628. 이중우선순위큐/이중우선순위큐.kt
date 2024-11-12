9ce4589750e31367f604467af8377572906e8544
wjsghk7664 <83314518+wjsghk7664@users.noreply.github.com>
2024-11-12 21:12:32 +0900

    [level 3] Title: 이중우선순위큐, Time: 94.62 ms, Memory: 106 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.kt
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.kt
@@ -2,59 +2,60 @@ import java.util.*
 class Solution {
     fun solution(operations: Array<String>): IntArray {
         var answer = intArrayOf()
-        var size=0
         
-        val upq=PriorityQueue<Node>(compareBy{-it.n})
-        val dpq=PriorityQueue<Node>(compareBy{it.n})
+        val q = PriorityQueue<Node>({a,b -> a.n - b.n})
+        val rq = PriorityQueue<Node>({a,b -> b.n - a.n})
         
         for(i in operations){
-            if(i == "D 1"){
-                if(size>0){
-                    getNonNull(upq)
-                    size--
-                }
-            }else if(i =="D -1"){
-                if(size>0){
-                    getNonNull(dpq)
-                    size--
-                }
+            val order = i.split(" ")
+            if(order[0]=="I"){
+                val node = Node(n = order[1].toInt())
+                q.add(node)
+                rq.add(node)
             }else{
-                i.split(" ")[1].toInt().let{
-                    val node=Node(it)
-                    upq.offer(node)
-                    dpq.offer(node)
-                    size++
+                if(order[1]=="-1"){
+                    while(q.isNotEmpty()){
+                        val poll = q.poll()
+                        if(!poll.isErased){
+                            poll.isErased = true
+                            break
+                        }
+                    }
+                }else{
+                    while(rq.isNotEmpty()){
+                        val poll = rq.poll()
+                        if(!poll.isErased){
+                            poll.isErased = true
+                            break
+                        }
+                    }
                 }
-                
             }
         }
         
-        if(size==1){
-            val tmp = getNonNull(upq)!!.n
-            return intArrayOf(tmp,tmp)
-        }else if(size>1){
-            answer+=getNonNull(upq)!!.n
-            answer+=getNonNull(dpq)!!.n 
+        while(rq.isNotEmpty()){
+            val poll = rq.poll()
+            if(!poll.isErased) {
+                answer+=poll.n
+                break
+            }
         }
-        else{
-            return intArrayOf(0,0)
+        while(q.isNotEmpty()){
+            val poll = q.poll()
+            if(!poll.isErased) {
+                answer+=poll.n
+                break
+            }
         }
-        
+        if(answer.isEmpty()){
+            answer+=0
+            answer+=0
+        }
         
         return answer
     }
     
-    fun getNonNull(pq:PriorityQueue<Node>):Node?{
-        var tmp:Node? =null
-        while(true){
-            tmp = pq.poll()
-            if(tmp.check){
-                tmp.check=false
-                break
-            }
-        }
-        return tmp
-    }
+    
 }
 
-data class Node(val n:Int, var check:Boolean=true)
+data class Node(var isErased:Boolean = false, val n:Int)
