e03e88c63856c27c8bddfce505d758f822f02a78
wjsghk7664 <83314518+wjsghk7664@users.noreply.github.com>
2024-11-12 21:50:17 +0900

    [level 2] Title: H-Index, Time: 22.40 ms, Memory: 61.8 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.kt
+++ 프로그래머스/2/42747. H－Index/H－Index.kt
@@ -1,14 +1,9 @@
 class Solution {
     fun solution(citations: IntArray): Int {
-        val arr=IntArray(10001){0} //인용횟수당 편수
-        for(i in citations){
-            arr[i]++
+        citations.sortDescending()
+        for(i in citations.indices){
+            if(i>=citations[i]) return i
         }
-        var sum=0
-        for(i in 10000 downTo 0){
-            sum+=arr[i]
-            if(sum>=i) return i
-        }
-        return -1
+        return citations.size
     }
 }
