35d64f363d4c19d581d9ef77e6dc0ba2dbee49c7
wjsghk7664 <83314518+wjsghk7664@users.noreply.github.com>
2024-11-13 00:49:32 +0900

    [level 1] Title: 모의고사, Time: 13.62 ms, Memory: 62.8 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.kt
+++ 프로그래머스/1/42840. 모의고사/모의고사.kt
@@ -1,47 +1,31 @@
 class Solution {
     fun solution(answers: IntArray): IntArray {
-        var answer = intArrayOf()
-        var s1=0
-        var s1a=intArrayOf(1,2,3,4,5)
-        var s2=0
-        var s2a=intArrayOf(2,1,2,3,2,4,2,5)
-        var s3=0
-        var s3a=intArrayOf(3,3,1,1,2,2,4,4,5,5)
+        val a = intArrayOf(1,2,3,4,5)
+        val b = intArrayOf(2,1,2,3,2,4,2,5)
+        val c = intArrayOf(3,3,1,1,2,2,4,4,5,5)
         
-        for(i:Int in 0..answers.size-1){
-            if(s1a[i%5]==answers[i]){
-                s1++
-            }
-            if(s2a[i%8]==answers[i]){
-                s2++
-            }
-            if(s3a[i%10]==answers[i]){
-                s3++
-            }
+        var ares = 0
+        var bres = 0
+        var cres = 0
+        
+        for((i,v) in answers.withIndex()){
+            var cura = a[i%a.size]
+            var curb = b[i%b.size]
+            var curc = c[i%c.size]
+            
+            if(cura==v) ares++
+            if(curb==v) bres++
+            if(curc==v) cres++
         }
-        if(s1!=0||s2!=0||s3!=0){
-            answer=if(s1==s2&&s2==s3){
-                intArrayOf(1,2,3)
-            }
-            else if(s1>s2&&s1>s3){
-                intArrayOf(1)
-            }
-            else if(s2>s1&&s2>s3){
-                intArrayOf(2)
-            }
-            else if(s3>s1&&s3>s2){
-                intArrayOf(3)
-            }
-            else if(s1==s2){
-                intArrayOf(1,2)
-            }
-            else if(s2==s3){
-                intArrayOf(2,3)
-            }
-            else{
-                intArrayOf(1,3)
-            }
-        }
+        
+        var answer = intArrayOf()
+        
+        val max = maxOf(ares,bres,cres)
+        
+        if(max == ares) answer+=1
+        if(max == bres) answer+=2
+        if(max == cres) answer+=3
+        
         return answer
     }
 }
