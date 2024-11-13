a26af477892f19663bf7178c8fabc2168461f0ae
wjsghk7664 <83314518+wjsghk7664@users.noreply.github.com>
2024-11-13 16:16:35 +0900

    [level 2] Title: 모음 사전, Time: 0.04 ms, Memory: 60.8 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.kt
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.kt
@@ -1,38 +1,71 @@
 class Solution {
     fun solution(word: String): Int {
-        var answer = 1
-        //AXXXX부터 시작
-        //순서는 X A E I O U순
-        var str=charArrayOf('A','X','X','X','X')
-        try{
-            while(true){
-            
-                if(word==str.joinToString().replace(", ", "").replace("X","")) return answer
-
-                var idx=4
-                while(true){
-                    if(str[idx-1]=='X') idx--
-                    else break
-                }
-                while(true){
-                    str[idx]=when(str[idx]){
-                        'X' -> 'A'
-                        'A' -> 'E' 
-                        'E' -> 'I'
-                        'I' -> 'O'
-                        'O' -> 'U'
-                        else ->'X'
-                    }
-                    if(str[idx]=='X'){
-                        idx--
-                    }else{
-                        break
-                    }
-                } 
-                answer++
-            }
-        }catch(e:Exception){
-            println("${str}")
+        var answer = 0
+        var first = 1 + 5 + 5*5 + 5*5*5 + 5*5*5*5
+        var second = 1 + 5 + 5*5 + 5*5*5
+        var third = 1 + 5 + 5*5
+        var fourth = 1 + 5
+        var fifth = 1
+        
+        answer+=when(word[0]){
+            'A' -> 0
+            'E' -> first
+            'I' -> first*2
+            'O' -> first*3
+            'U' -> first*4
+            else -> 0
+        }
+        
+        answer+=1
+        
+        if(word.length==1) return answer
+        
+        answer+=when(word[1]){
+            'A' -> 0
+            'E' -> second
+            'I' -> second*2
+            'O' -> second*3
+            'U' -> second*4
+            else -> 0
+        }
+        
+        answer+=1
+        
+        if(word.length==2) return answer
+        
+        answer+=when(word[2]){
+            'A' -> 0
+            'E' -> third
+            'I' -> third*2
+            'O' -> third*3
+            'U' -> third*4
+            else -> 0
+        }
+        
+        answer+=1
+        
+        if(word.length==3) return answer
+        
+        answer+=when(word[3]){
+            'A' -> 0
+            'E' -> fourth
+            'I' -> fourth*2
+            'O' -> fourth*3
+            'U' -> fourth*4
+            else -> 0
+        }
+        
+        answer+=1
+        
+        if(word.length==4) return answer
+        
+        answer+=when(word[4]){
+            'A' -> 1
+            'E' -> 2
+            'I' -> 3
+            'O' -> 4
+            'U' -> 5
+            else -> 0
         }
         
         return answer
