bb992940a01b6fb023c22dc65df08f877e9291a0
wjsghk7664 <83314518+wjsghk7664@users.noreply.github.com>
2024-11-14 11:23:29 +0900

    [level 3] Title: N으로 표현, Time: 69.12 ms, Memory: 76 MB -BaekjoonHub

--- 프로그래머스/3/42895. N으로 표현/N으로 표현.kt
+++ 프로그래머스/3/42895. N으로 표현/N으로 표현.kt
@@ -4,31 +4,28 @@ class Solution {
         
         val dp = Array(9){HashSet<Int>()}
         
-        //초기화
-        var tmp = N
+        //초기값으로 N,NN,NNN...추가
+        var tmpN=N
         for(i in 1..8){
-            dp[i]+=tmp
-            tmp=10*tmp+N
+            dp[i]+=tmpN
+            tmpN=tmpN*10+N
         }
         
-        if(number==N) return 1
-        
-        for(i in 2..8){
-            var prev=1
-            while(prev<i){
-                for(j in dp[prev]){
-                    for(k in dp[i-prev]){
-                        dp[i]+=j+k
-                        dp[i]+=j-k
-                        dp[i]+=j*k
-                        if(k!=0) dp[i]+=j/k
+        for(i in 1..8){
+            for(j in 1..i){
+                if(i+j>8) break
+                for(v1 in dp[i]){
+                    for(v2 in dp[j]){
+                        dp[i+j]+=hashSetOf(v1+v2, v1-v2, v2-v1,v1*v2)
+                        if(v1!=0) dp[i+j]+=v2/v1
+                        if(v2!=0) dp[i+j]+=v1/v2
                     }
                 }
-                prev++
             }
+        }
+        for(i in 1..8){
             if(dp[i].contains(number)) return i
         }
-        
         return -1
     }
 }
