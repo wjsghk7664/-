df44dd1fb07f6b04224162cfd7e7832b105b3e8d
wjsghk7664 <83314518+wjsghk7664@users.noreply.github.com>
2024-11-14 12:22:18 +0900

    [level 3] Title: 여행경로, Time: 189.08 ms, Memory: 97 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/README.md
+++ 프로그래머스/3/43164. 여행경로/README.md
@@ -1,10 +1,10 @@
 # [level 3] 여행경로 - 43164 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43164?language=kotlin) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43164) 
 
 ### 성능 요약
 
-메모리: 84.7 MB, 시간: 95.98 ms
+메모리: 97 MB, 시간: 189.08 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 08월 07일 10:37:26
+2024년 11월 14일 12:22:15
 
 ### 문제 설명
 
--- 프로그래머스/3/43164. 여행경로/여행경로.kt
+++ 프로그래머스/3/43164. 여행경로/여행경로.kt
@@ -1,42 +1,41 @@
 class Solution {
     
-    data class Node(val route:Array<String>, val used:BooleanArray)
-    
     fun solution(tickets: Array<Array<String>>): Array<String> {
         var answer = arrayOf<String>()
         
-        val q=ArrayDeque<Node>()
+        val q = ArrayDeque<Node>()
         
         q.addLast(Node(arrayOf("ICN"),BooleanArray(tickets.size){false}))
         
         while(q.isNotEmpty()){
             val cur = q.removeFirst()
             
-            if(!cur.used.contains(false)){
-                answer = if(answer.isEmpty()) cur.route else compares(answer,cur.route)
+            if(cur.used.filter{!it}.isEmpty()) {
+                answer = checkOrder(answer,cur.cur)
                 continue
             }
             
-            for(i in tickets.indices){
-                if(!cur.used[i]&&tickets[i][0]==cur.route.last()){
-                    val new=Node(cur.route+tickets[i][1],cur.used.copyOf().also{it[i]=true})
+            for((i,v) in tickets.withIndex()){
+                if(v[0]==cur.cur.last()&&!cur.used[i]){
+                    val new = Node(cur.cur.copyOf()+v[1],cur.used.copyOf().apply{this[i]=true})
                     q.addLast(new)
                 }
             }
         }
         
-        
-        
         return answer
     }
     
-    fun compares(str1:Array<String>,str2:Array<String>):Array<String>{
-        for(i in str1.indices){
-            val tmp = str1[i].compareTo(str2[i])
-            if(tmp!=0){
-                return if(tmp<0) str1 else str2
+
+    fun checkOrder(a:Array<String>, b:Array<String>):Array<String>{
+        if(a.isEmpty()) return b
+        for(i in a.indices){
+            if(a[i]!=b[i]){
+                if(a[i]==arrayOf(a[i],b[i]).sorted()[0]) return a else return b
             }
         }
-        return str1
+        return a
     }
 }
+
+class Node(val cur:Array<String>, val used:BooleanArray)
