c8a52b5e5ce5c8735172f7123acb9753059b6688
wjsghk7664 <83314518+wjsghk7664@users.noreply.github.com>
2024-11-24 12:36:30 +0900

    [level 3] Title: 입국심사, Time: 103.79 ms, Memory: 69.4 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.kt
+++ 프로그래머스/3/43238. 입국심사/입국심사.kt
@@ -2,41 +2,36 @@ class Solution {
     fun solution(n: Int, times: IntArray): Long {
         var answer: Long = 0
         
-        //기준: 각 심사관별 총 심사시간중 최대값이 최소가 되는 것
-        //총 심사시간: 인원을 분배하여 심사기간에 곱해준다.
-        //최대 시간은 심사관 최대시간*입국심사 기다리는 전체 인원수
-        //각 루프별로 체크해서 최대시간을 이진 탐색으로 찾아간다.
+        //총 심사시간으로 이진탐색
+       
         
         times.sort()
         
-        var left=0L
-        var right=times.last()*n.toLong()
+        var left = 0L
+        var right = n.toLong()*times.last().toLong()
         
         while(left<right){
-            val mid=(left+right)/2L
             var total = 0L
+            val mid = (left+right)/2L
             
-            //최대값 이하중 최대가 되도록 인원분배
+            //주어진 시간 mid동안 각 심사관이 심사할수있는 인원수를 구해 더해줌
             for(i in times){
                 total+=mid/i.toLong()
             }
             
-            //총 인원수와 같으면 최대값을 더 내렸을때 총인원수가 같을수 있다
-            //예를 들어 [3,5] 11이면 3명 2명으로 총 5명인데 10이면 3명 2명으로 여전히 5명임
-            if(total==n.toLong()){
-                right=mid
-            } 
-            //총 인원수가 n보다 작으면 최대값을 늘려야 한다
-            else if(total<n.toLong()){
-                left=mid+1
-            } 
-            //총 인원수가 n보다 크면 최대값을 줄여야 한다.
+            //총 인원수가 n보다 크면 시간을 줄임
+            if(total>n.toLong()){
+                right = mid
+            }else if(total<n.toLong()){
+                left = mid+1
+            }
+            //총 인원수가 n과 같으면 최대값을 내려도 같을 수 있음
+            //mid가 11 [3,5]일시 인원수는 3,2로 5명인데 10 [3,5]여도 3,2로 5명 
             else{
-                right=mid
+                right = mid
             }
         }
         
-        
-        return right
+        return left
     }
 }
